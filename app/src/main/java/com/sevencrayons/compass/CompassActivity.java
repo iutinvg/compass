a6b55ba2a797d6656ad35c20ebde0675e9dd8ec3
Viacheslav Iutin <iutinvg@gmail.com>
2018-04-12 18:22:50 +0300

    make sure the project is easy to build with the latest version of tools and without too much sweat

--- app/src/main/java/com/sevencrayons/compass/CompassActivity.java
+++ app/src/main/java/com/sevencrayons/compass/CompassActivity.java
@@ -1,14 +1,12 @@
 package com.sevencrayons.compass;
 
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ImageView;
 import android.util.Log;
 
 
-public class CompassActivity extends ActionBarActivity {
+public class CompassActivity extends AppCompatActivity {
 
     private static final String TAG = "CompassActivity";
 
@@ -48,26 +46,4 @@ public class CompassActivity extends ActionBarActivity {
         Log.d(TAG, "stop compass");
         compass.stop();
     }
-
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_compass, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
 }
