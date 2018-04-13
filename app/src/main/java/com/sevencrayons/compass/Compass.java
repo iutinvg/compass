b7a746abbd0eb7d92e94fad0d272d20ee3194c60
Viacheslav Iutin <iutinvg@gmail.com>
2018-04-13 10:33:38 +0300

    reformat file for easier merging later

--- app/src/main/java/com/sevencrayons/compass/Compass.java
+++ app/src/main/java/com/sevencrayons/compass/Compass.java
@@ -16,97 +16,98 @@ public class Compass implements SensorEventListener {
     public interface CompassListener {
         void onNewAzimuth(float azimuth);
     }
+
     private CompassListener listener;
 
     private SensorManager sensorManager;
-	private Sensor gsensor;
-	private Sensor msensor;
-	private float[] mGravity = new float[3];
-	private float[] mGeomagnetic = new float[3];
-	private float azimuth;
-	private float azimuthFix;
+    private Sensor gsensor;
+    private Sensor msensor;
+    private float[] mGravity = new float[3];
+    private float[] mGeomagnetic = new float[3];
+    private float azimuth;
+    private float azimuthFix;
 
-	public Compass(Context context) {
-		sensorManager = (SensorManager) context
-				.getSystemService(Context.SENSOR_SERVICE);
-		gsensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-		msensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
-	}
+    public Compass(Context context) {
+        sensorManager = (SensorManager) context
+                .getSystemService(Context.SENSOR_SERVICE);
+        gsensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        msensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+    }
 
-	public void start() {
-		sensorManager.registerListener(this, gsensor,
-				SensorManager.SENSOR_DELAY_GAME);
-		sensorManager.registerListener(this, msensor,
-				SensorManager.SENSOR_DELAY_GAME);
-	}
+    public void start() {
+        sensorManager.registerListener(this, gsensor,
+                SensorManager.SENSOR_DELAY_GAME);
+        sensorManager.registerListener(this, msensor,
+                SensorManager.SENSOR_DELAY_GAME);
+    }
 
-	public void stop() {
-		sensorManager.unregisterListener(this);
-	}
+    public void stop() {
+        sensorManager.unregisterListener(this);
+    }
 
-	public void setAzimuthFix(float fix) {
-		azimuthFix = fix;
-	}
+    public void setAzimuthFix(float fix) {
+        azimuthFix = fix;
+    }
 
-	public void resetAzimuthFix() {
-		setAzimuthFix(0);
-	}
+    public void resetAzimuthFix() {
+        setAzimuthFix(0);
+    }
 
-	public void setListener(CompassListener l) {
+    public void setListener(CompassListener l) {
         listener = l;
     }
 
-	@Override
-	public void onSensorChanged(SensorEvent event) {
-		final float alpha = 0.97f;
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        final float alpha = 0.97f;
 
-		synchronized (this) {
-			if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+        synchronized (this) {
+            if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
 
-				mGravity[0] = alpha * mGravity[0] + (1 - alpha)
-						* event.values[0];
-				mGravity[1] = alpha * mGravity[1] + (1 - alpha)
-						* event.values[1];
-				mGravity[2] = alpha * mGravity[2] + (1 - alpha)
-						* event.values[2];
+                mGravity[0] = alpha * mGravity[0] + (1 - alpha)
+                        * event.values[0];
+                mGravity[1] = alpha * mGravity[1] + (1 - alpha)
+                        * event.values[1];
+                mGravity[2] = alpha * mGravity[2] + (1 - alpha)
+                        * event.values[2];
 
-				// mGravity = event.values;
+                // mGravity = event.values;
 
-				// Log.e(TAG, Float.toString(mGravity[0]));
-			}
+                // Log.e(TAG, Float.toString(mGravity[0]));
+            }
 
-			if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
-				// mGeomagnetic = event.values;
+            if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+                // mGeomagnetic = event.values;
 
-				mGeomagnetic[0] = alpha * mGeomagnetic[0] + (1 - alpha)
-						* event.values[0];
-				mGeomagnetic[1] = alpha * mGeomagnetic[1] + (1 - alpha)
-						* event.values[1];
-				mGeomagnetic[2] = alpha * mGeomagnetic[2] + (1 - alpha)
-						* event.values[2];
-				// Log.e(TAG, Float.toString(event.values[0]));
+                mGeomagnetic[0] = alpha * mGeomagnetic[0] + (1 - alpha)
+                        * event.values[0];
+                mGeomagnetic[1] = alpha * mGeomagnetic[1] + (1 - alpha)
+                        * event.values[1];
+                mGeomagnetic[2] = alpha * mGeomagnetic[2] + (1 - alpha)
+                        * event.values[2];
+                // Log.e(TAG, Float.toString(event.values[0]));
 
-			}
+            }
 
-			float R[] = new float[9];
-			float I[] = new float[9];
-			boolean success = SensorManager.getRotationMatrix(R, I, mGravity,
-					mGeomagnetic);
-			if (success) {
-				float orientation[] = new float[3];
-				SensorManager.getOrientation(R, orientation);
-				// Log.d(TAG, "azimuth (rad): " + azimuth);
-				azimuth = (float) Math.toDegrees(orientation[0]); // orientation
-				azimuth = (azimuth + azimuthFix + 360) % 360;
-				// Log.d(TAG, "azimuth (deg): " + azimuth);
+            float R[] = new float[9];
+            float I[] = new float[9];
+            boolean success = SensorManager.getRotationMatrix(R, I, mGravity,
+                    mGeomagnetic);
+            if (success) {
+                float orientation[] = new float[3];
+                SensorManager.getOrientation(R, orientation);
+                // Log.d(TAG, "azimuth (rad): " + azimuth);
+                azimuth = (float) Math.toDegrees(orientation[0]); // orientation
+                azimuth = (azimuth + azimuthFix + 360) % 360;
+                // Log.d(TAG, "azimuth (deg): " + azimuth);
                 if (listener != null) {
                     listener.onNewAzimuth(azimuth);
                 }
-			}
-		}
-	}
+            }
+        }
+    }
 
-	@Override
-	public void onAccuracyChanged(Sensor sensor, int accuracy) {
-	}
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
 }
